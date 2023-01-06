fc15937c0fd9eba9dacc9a57979d3ac141663e87
jameslong2 <jaumecarrion@gmail.com>
2023-01-06 19:27:37 +0100

    sfdfd
    sdfsdf

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from './node_modules/three/build/three.module.js';
 import * as CANNON from 'cannon-es';
 import CannonDebugger from 'cannon-es-debugger';
 import CannonUtils from 'cannon-utils';
